0d335a4efeef7628ea26fb141d576d0dbb38601b
eyansibi <eya.nsibi@esprit.tn>
2025-04-05 18:13:23 +0100

    add some functionality

--- ChambreMS/src/main/java/tn/esprit/chambrems/ChambreRepository.java
+++ ChambreMS/src/main/java/tn/esprit/chambrems/ChambreRepository.java
@@ -3,6 +3,11 @@ package tn.esprit.chambrems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ChambreRepository extends JpaRepository<Chambre, Integer> {
+public interface ChambreRepository extends JpaRepository<Chambre, Long> {
+    List<Chambre> findByTypeAndDisponibiliteTrue(TypeChambre type);
+    List<Chambre> findByPrixParNuitBetween(double min, double max);
+
 }
